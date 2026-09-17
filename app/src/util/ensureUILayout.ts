f7b1e72664a305260f709e0407785241c1d623e3
Sunan869 <1052823667@qq.com>
2026-09-17 10:10:03 +0800

    feat(aiks): apply embedded dock policy before layout registration

--- app/src/util/ensureUILayout.ts
+++ app/src/util/ensureUILayout.ts
@@ -1,7 +1,12 @@
 import {Constants} from "../constants";
+import {applyAiksWorkbenchLayoutPolicy} from "../aiks/runtime";
+import {isAiksEmbedded} from "../aiks/profile";
 
 export const ensureUILayout = () => {
     if (!window.siyuan.config.uiLayout?.left) {
         window.siyuan.config.uiLayout = JSON.parse(JSON.stringify(Constants.SIYUAN_EMPTY_LAYOUT));
+    }
+    if (isAiksEmbedded()) {
+        applyAiksWorkbenchLayoutPolicy(window.siyuan.config.uiLayout);
     }
 };
