39f47e44cfab9ab56131caba8ba42eec89a8e0c4
Sunan869 <1052823667@qq.com>
2026-09-17 09:52:09 +0800

    feat(aiks): export native workbench contracts

--- app/src/aiks/index.ts
+++ app/src/aiks/index.ts
@@ -5,3 +5,13 @@ export {
     resolveAiksEmbeddedProfile,
     type AiksEmbeddedProfile,
 } from "./profile";
+export {AIKS_ROOTS, type AiksRootPresentation} from "./roots";
+export {
+    AIKS_WORKBENCH_LAYOUT,
+    type AiksAuxiliaryTab,
+    type AiksToolbarAction,
+    type AiksWorkbenchLayoutModel,
+    type AiksWorkbenchMode,
+    type AiksWorkbenchPane,
+} from "./layout";
+export {AIKS_SEARCH, type AiksSearchModel, type AiksSearchScope} from "./search";
